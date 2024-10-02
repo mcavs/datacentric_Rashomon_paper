f5ca6ff8c3ee138224b83fa0f11e760d1b227fec
Mustafa CAVUS <45355486+mcavs@users.noreply.github.com>
2024-10-02 22:07:54 +0300

    Add files via upload

---
+++ import_dataset.R
@@ -0,0 +1,19 @@
+setwd("/Volumes/LaCie/Data-centric Rashomon/datasets")
+csv_files <- list.files(pattern = "\\.csv$")
+
+datasets <- NULL
+for (file in csv_files) {
+  data <- read.csv(file)
+  # Transform the response variable to a factor variable
+  data$Class <- as.factor(data$Class)
+  
+  char_columns <- sapply(data, is.character)
+  if (any(char_columns)) {
+    char_column_names <- names(data)[char_columns]
+    data[char_column_names] <- lapply(data[char_column_names], function(x) as.numeric(as.factor(x)))
+  }
+  
+  datasets[[file]] <- data
+  assign(gsub("\\.csv$", "", file), data)
+}
+
